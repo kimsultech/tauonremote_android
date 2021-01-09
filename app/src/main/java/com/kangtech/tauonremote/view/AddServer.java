277c3fdf9edf6f2914fa32fbb7f5cc357957e0d9
sultannamja <ssx20010531@gmail.com>
2021-01-09 12:48:43 +0700

    Added Manual add Server, Searching Server Update

--- app/src/main/java/com/kangtech/tauonremote/view/AddServer.java
+++ app/src/main/java/com/kangtech/tauonremote/view/AddServer.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
@@ -19,9 +20,12 @@ import android.provider.SyncStateContract;
 import android.text.format.Formatter;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.dinuscxj.refresh.RecyclerRefreshLayout;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.kangtech.tauonremote.R;
@@ -38,6 +42,7 @@ import java.lang.ref.WeakReference;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.concurrent.Delayed;
 
 public class AddServer extends AppCompatActivity {
@@ -50,7 +55,12 @@ public class AddServer extends AppCompatActivity {
     public static RecyclerRefreshLayout refreshLayout;
     public NetworkSniffTask nettask;
 
+    private SharedPreferences.Editor editor;
+
     private Toolbar toolbar;
+
+    private TextInputEditText tieIP;
+    private Button btnRemote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +72,26 @@ public class AddServer extends AppCompatActivity {
 
         refreshLayout = findViewById(R.id.srl_server);
         toolbar = findViewById(R.id.toolbar_addserver);
+        tieIP = findViewById(R.id.tie_ip);
+        btnRemote = findViewById(R.id.btn_remote);
+
+        btnRemote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Objects.requireNonNull(tieIP.getText()).length() == 0) {
+                    Toast.makeText(AddServer.this, "Enter IP address", Toast.LENGTH_SHORT).show();
+                } else {
+                    editor = getSharedPreferences("tauon_remote", MODE_PRIVATE).edit();
+                    editor.putBoolean("set_server", true);
+                    editor.putString("ip", tieIP.getText().toString());
+                    editor.apply();
+
+                    Intent intent = new Intent(AddServer.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
 
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
